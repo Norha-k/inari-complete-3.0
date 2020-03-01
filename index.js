fd5007a4de3509438e56d6c3f97ac437b557d006
Norha-k <hemantkanojiya1999@gmail.com>
2020-03-01 18:11:04 +0530

    serviceAccount fix

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ http.createServer().listen(port);
 const firebase = require('firebase/app');
 const fieldValue = require('firebase-admin').firestore.fieldValue;
 const admin = require('firebase-admin');
-const serviceAccount  = require('./ServiceAccount.json');
+const serviceAccount  = require('./serviceAccount.json');
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
